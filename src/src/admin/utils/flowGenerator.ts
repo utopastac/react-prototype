39a3bd88318a3161a855b1d5a3ceb720ba861e7d
Peter Wright <utopastac@gmail.com>
2025-11-02 14:00:33 -0500

    Removing LLM generation nonsense

--- src/src/admin/utils/flowGenerator.ts
+++
@@ -1,7 +0,0 @@
-export interface GeneratedFlow {
-  layouts: any[];
-  names: string[];
-  layoutPositions: Record<string, { row: number; col: number }>;
-  gridRows: number;
-  gridCols: number;
-}
