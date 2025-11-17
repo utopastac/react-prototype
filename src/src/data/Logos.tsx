0591c909fed96de7c6c6ee4639b221b5e558f373
Peter Wright <utopastac@gmail.com>
2025-11-17 18:16:01 +0000

    Adding the logo

---
+++ src/src/data/Logos.tsx
@@ -0,0 +1,43 @@
+import LightInSize4 from "src/assets/logo/LightIn/Size4.svg";
+import LightInSize5 from "src/assets/logo/LightIn/Size5.svg";
+import LightInSize6 from "src/assets/logo/LightIn/Size6.svg";
+import LightLogoSize7 from "src/assets/logo/LightLogo/Size7.svg";
+import LightLogoSize8 from "src/assets/logo/LightLogo/Size8.svg";
+import LightLogoSize9 from "src/assets/logo/LightLogo/Size9.svg";
+import DarkInSize4 from "src/assets/logo/DarkIn/Size4.svg";
+import DarkInSize5 from "src/assets/logo/DarkIn/Size5.svg";
+import DarkInSize6 from "src/assets/logo/DarkIn/Size6.svg";
+import DarkLogoSize7 from "src/assets/logo/DarkLogo/Size7.svg";
+import DarkLogoSize8 from "src/assets/logo/DarkLogo/Size8.svg";
+import DarkLogoSize9 from "src/assets/logo/DarkLogo/Size9.svg";
+
+export const In = {
+  Dark:   {
+      Size4: DarkInSize4,
+      Size5: DarkInSize5,
+      Size6: DarkInSize6,
+  },
+  Light:   {
+      Size4: LightInSize4,
+      Size5: LightInSize5,
+      Size6: LightInSize6,
+  },
+};
+export const Logo = {
+  Dark:   {
+      Size7: DarkLogoSize7,
+      Size8: DarkLogoSize8,
+      Size9: DarkLogoSize9,
+  },
+  Light:   {
+      Size7: LightLogoSize7,
+      Size8: LightLogoSize8,
+      Size9: LightLogoSize9,
+  },
+};
+
+export default {
+  In,
+  Logo
+};
+
