6a318d3379102d02ef35f98ea24e80a05349a636
Peter Wright <utopastac@gmail.com>
2025-11-17 23:18:04 +0000

    Fixing toast styling

--- src/src/components/Toast/index.tsx
+++ src/src/components/Toast/index.tsx
@@ -1,27 +1,36 @@
 import React from "react";
 import Icon, { IconPropMeta } from "src/components/Icon";
+import Button from "src/components/Buttons/Button";
+import * as Icons from "src/data/AllIcons";
 import styles from "./index.module.sass";
 
 export interface ToastProps {
   headline: string;
   body?: string;
-  button?: string;
+  onClose?: () => void;
   icon?: string
 }
 
-const Toast: React.FC<ToastProps> = ({ headline, body, button, icon }) => {
+const Toast: React.FC<ToastProps> = ({ headline, body, onClose, icon }) => {
 
   return (
     <div className={styles.Main}>
       <div>
         { icon && (
-          <div className={styles.icon}><Icon icon={icon} size="24" color="inverse" /></div>
+          <div className={styles.icon}><Icon icon={icon} size="24" color="standard" /></div>
         )}
         <div className={styles.content}>
           <h4>{headline}</h4>
           {body && <p>{body}</p> }
         </div>
-        {button && <h5>{button}</h5>}
+        <div className={styles.closeButton}>
+          <Button
+            type="ghost"
+            size="small"
+            icon={{ icon: Icons.System.CloseSmall, size: "16", color: "standard" }}
+            onClick={onClose}
+          />
+        </div>
       </div>
     </div>
   );
@@ -32,6 +41,6 @@ export default Toast;
 export const ToastPropMeta = {
   headline: { type: 'string', label: 'Headline' },
   body: { type: 'string', label: 'Body' },
-  button: { type: 'string', label: 'Button' },
+  onClose: { type: 'function', label: 'onClose (not editable)' },
   icon: IconPropMeta.icon
 };
